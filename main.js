24b9aac056c3f210562196d7742999aaac92b9b3
himapolina <67219127+himapolina@users.noreply.github.com>
2022-01-21 09:10:12 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+$(function(){
+    $('#photu').hover(function(){
+        $('#photu-bg').css('transform', 'rotate(6deg)');
+    })
+    $('#photu').mouseleave(function(){
+        $('#photu-bg').css('transform', 'rotate(0deg)');
+    })
+})
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+
+$('#cool').mousemove(function(event) {
+    windowWidth = $('body').width();
+    windowHeight = $('body').height();
+    
+    mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
+    mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
+    
+    $('body').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #33363b, #232323)');
+  });
